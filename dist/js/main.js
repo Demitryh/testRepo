41ce9de5681d8768389c9ac2c71dcedb66ce619f
Demitryh <dmytro.haras@gmail.com>
2016-05-20 23:22:08 +0300

    Add files via upload

---
+++ dist/js/main.js
@@ -0,0 +1,13 @@
+function addRow(n,name,shortname,code,ED,state) {
+    var enter=[n,name,shortname,code,ED,state],//
+        tabContent= "<tr>";
+    for (var i=0;i<=10;i++)
+    {
+        tabContent+="<td>"+enter[i]+"</td> " +
+            "<td>"+enter[i]+"</td>" +
+            "<td>"+enter[i]+"</td>" +
+            "<td>"+enter[i]+"</td>" +
+            "<td>"+enter[i]+"</td>" ;
+        tabContent+="</tr>";}
+    example.innerHTML+=tabContent;
+}
