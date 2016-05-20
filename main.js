45038d5615dc93d72d3988a9159aa680123b2a1c
Demitryh <dmytro.haras@gmail.com>
2016-05-20 22:56:21 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+Papa.parse(shortlist.txt,{
+        complete: function(results) {
+            console.log("Finished:", results.data);
+        }
+    }
+);
+function addRow(n,name,shortname,code,ED,state) {
+    var enter=[n,name,shortname,code,ED,state],
+        tabContent= "<tr>";
+    for (var i=0;i<=10;i++)
+    {
+        tabContent+="<td>"+results[i]+"</td> " +
+            "<td>"+results[i]+"</td>" +
+            "<td>"+results[i]+"</td>" +
+            "<td>"+results[i]+"</td>" +
+            "<td>"+results[i]+"</td>" ;
+        tabContent+="</tr>";}
+        example.innerHTML+=tabContent;
+}
+
+
